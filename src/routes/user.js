fe775d2cdade0d50770c1e98d34b59a45ca3ecc8
Juan José Rojas <juanjoserojas@MacBook-Pro-de-Juan.local>
2018-07-19 07:26:01 -0500

    build user index route complete

--- src/routes/user.js
+++ src/routes/user.js
@@ -16,48 +16,44 @@ const User = require('../models/user');
 
 
 router.get('', passport.authenticate('jwt', {session:false}),
-          authCtrl.roleAuthorization(['admin']),
+          authCtrl.roleAuthorization(['admin','moderator']),
           (req,res) => {
+            const query = {};
+            let page = req.query.page? Number(req.query.page) : 1;
+            if (req.user.role == 'moderator') {
+              query.role = 'currentUser';
+            }
             if (req.query.searchUsername) {
               let searchUsername = fuzzySearch.escapeRegex(req.query.searchUsername.toString());
-              let page = req.query.page? Number(req.query.page) : 1;
-              const regex = new RegExp(searchUsername, 'gi');
-              return User.count({username: regex})
-                     .exec((er,count) => {
-                      if (er) return res.json({success: false, msg: `Something was wrong` });
-                      if (!count) return res.json({success: true, users: null });
-                      if (Math.ceil(count/opcPage.perPage) < page ) {
-                        page = Math.ceil(count/opcPage.perPage);
-                      }
-                      return User.find({ username: regex })
-                             .skip(opcPage.perPage*(page-1))
-                             .limit(opcPage.perPage)
-                             .exec ((err, foundUsers) => {
-                                      if(err) return res.json({success: false, msg: `Something was wrong` });
-                                      return res.json({success: true, users: foundUsers, page: page, count: count });
-                                   });
-                          });
+              let regex = new RegExp(searchUsername, 'gi');
+              query.username = regex;
             }
             if (req.query.searchEmail) {
               let searchEmail = fuzzySearch.escapeRegex(req.query.searchEmail.toString());
-              let page = req.query.page? Number(req.query.page) : 1;
-              const regex = new RegExp(searchEmail, 'gi');
-              return User.count({ email: regex})
-                     .exec((er,count) => {
-                       if (er) return res.json({success: false, msg: `Something was wrong` });
-                       if (!count) return res.json({success: true, users: null });
-                       if (Math.ceil(count/opcPage.perPage) < page ) {
-                         page = Math.ceil(count/opcPage.perPage);
-                       }
-                       return User.find({ email: regex })
-                              .skip(opcPage.perPage*(page-1))
-                              .limit(opcPage.perPage)
-                              .exec((err, foundUsers) => {
-                                      if(err) return res.json({success: false, msg: `Something was wrong` });
-                                      return res.json({success: true, users: foundUsers, page: page, count: count});
-                                    });
-                          });
+              let regex = new RegExp(searchEmail, 'gi');
+              query.email = regex;
            }
+           if (req.query.activate || req.query.inactive) {
+             query.activate = req.activate? true: false;
+           }
+           if (!query.role && req.query.role) {
+             query.role = req.query.role;
+           }
+           return User.count(query)
+                  .exec((er,count) => {
+                   if (er) return res.json({success: false, msg: `Something was wrong` });
+                   if (!count) return res.json({success: true, users: null });
+                   if (Math.ceil(count/opcPage.perPage) < page ) {
+                     page = Math.ceil(count/opcPage.perPage);
+                   }
+                   return User.find(query)
+                          .skip(opcPage.perPage*(page-1))
+                          .limit(opcPage.perPage)
+                          .exec ((err, foundUsers) => {
+                                   if(err) return res.json({success: false, msg: `Something was wrong` });
+                                   return res.json({success: true, users: foundUsers, page: page, count: count });
+                                });
+                       });
 });
 
 module.exports = router;
